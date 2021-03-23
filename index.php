5cae451c089f124f286a43e6bcbe36bb2d294e85
tainlagcd191069 <81226099+tainlagcd191069@users.noreply.github.com>
2021-03-23 16:31:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="planpage.css">
+</head>
+<body>
+    <div id="navigation">
+        <nav id="topmenu">
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Tutorials</a></li>
+              <li><a href="#">About</a></li>
+              <li><a href="#">Newsletter</a></li>
+              <li><a href="#">Contact</a></li>
+    <section>
+        <article>
+            <h1> Inspection </h1>
+            <p>Mirum est notare quam littera gothica, quam nunc 
+                <img src="arsenal-15-16-third-kit-2.jpeg" alt="" class="img_te">
+            <div class="price"> $300.00 </div>
+            <ul>
+                <li>Auto Product Liability</li>
+                <li>Crib Recall </li>
+                <li>Defective Airbags</li>
+                <li>Defective medical device</li>
+                <li>Food Poisoning</li>
+            <ul>
+            <button>Contact me</button>
+            
+          </article> 
+          <article>
+          <h1> Inspection </h1>
+            <p>Mirum est notare quam littera gothica, quam nunc 
+                <img src="arsenal-15-16-away-kit-1.jpeg" alt="" class="img_te">  
+            <div class="price"> $270.00 </div>
+            <ul>
+                <li>Auto Product Liability</li>
+                <li>Crib Recall </li>
+                <li>Defective Airbags</li>
+                <li>Defective medical device</li>
+                <li>Food Poisoning</li>
+            <ul>
+            <button>Contact me</button>
+           </article>
+           <article>
+            <h1> Inspection </h1>
+              <p>Mirum est notare quam littera gothica, quam nunc   
+                <img src="arsenal-15-16-home-kit-1.jpeg" alt="" class="img_te">
+              <div class="price"> $450.00 </div>
+              <ul>
+                  <li>Auto Product Liability</li>
+                  <li>Crib Recall </li>
+                  <li>Defective Airbags</li>
+                  <li>Defective medical device</li>
+                  <li>Food Poisoning</li>
+              <ul>
+              <button>Contact me</button>
+             </article>
+        </section>           
+</body>
+</html>
